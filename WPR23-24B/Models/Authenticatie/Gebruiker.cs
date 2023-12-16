b35f3058fdd152c64b9c8bf4be198c52351fb9fb
DKnightAnon <a.r.c.delgado@outlook.com>
2023-12-16 12:25:36 +0100

    Model classes toegevoegd voor authenticatie. Bedrij-gerelateerde klassen moeten nog. Ook zijn er geen controllers.

---
+++ WPR23-24B/Models/Authenticatie/Gebruiker.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+
+namespace WPR23_24B.Models.Authenticatie
+{
+    public class Gebruiker : IdentityUser
+    {
+        [Required(ErrorMessage ="Een naam moet ingevuld worden.")]
+        [StringLength(50)]
+        public string Voornaam { get; set; }
+
+
+        [Required(ErrorMessage = "Een naam moet ingevuld worden.")]
+        [StringLength(50)]
+        public string Achternaam { get; set; }
+
+        [Required(ErrorMessage = "Een Emailadres moet ingevuld worden.")]
+        [EmailAddress]
+        public string Email {  get; set; }
+
+        [Required]
+        //RegularExpression limits the allowed inputs to the specified characters
+        [RegularExpression("^[0-9]{10}$", ErrorMessage ="Telefoonnummer moet 10 cijfers lang zijn, en starten met 06 .")]
+        public string Telefoon_Nummer { get; set; }
+
+        //Used for concurrency. Concurrency is a technique used to prevent two suers from updating the same record at the same time.
+        [Timestamp]
+        public byte[] Timestamp { get; set; }
+
+    }
+}
---
+++ WPR23-24B/Models/Authenticatie/LoginDTO.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WPR23_24B.Models.Authenticatie
+{
+    //Data Transfer Object to allow the logging
+    public class LoginDTO
+    {
+        [EmailAddress]
+        [Required(ErrorMessage = "Emailadress is verplicht.")]
+        public string? UserName { get; init; }
+
+        
+        [Required(ErrorMessage = "Wachtwoord is verplicht.")]
+        public string? Password { get; init; }
+    }
+}
