d75c8f9b913a888c85f9de023a2ae89c4a8f931b
Lossttt <lfatmi.ayoub@gmail.com>
2024-01-14 21:51:24 +0100

    Zichbare meldingen bij ongeldige input, extra accessibility toegepast en in het algemeen wat dingen aangepast

--- WPR23-24B/ClientApp/src/components/SignUpComponents/CommercialApproach.js
+++ WPR23-24B/ClientApp/src/components/SignUpComponents/CommercialApproach.js
@@ -1,50 +1,81 @@
-// CommercialApproach.js
 import React from "react";
 import "./SignUpStyle.css";
+import { validateApproach } from "./SignUpValidation";
 
 const CommercialApproach = ({ onPrev, onSubmit, values, handleChange }) => {
+  const [validationErrors, setValidationErrors] = React.useState({});
+
+  const handleValidation = () => {
+    const errors = validateApproach(values);
+
+    if (Object.keys(errors).length > 0) {
+      setValidationErrors(errors);
+      return false;
+    }
+    return true;
+  };
+
   return (
     <div>
       <div className="triangle-background"></div>
       <div style={styles.container}>
         <h2 style={styles.header}>Toestaan benaderingen</h2>
         <div style={styles.checkboxContainer}>
-          <label style={styles.checkboxLabel}>
-            Sta commerciële benadering toe:
+          <label style={styles.checkboxLabel} htmlFor="BenaderingCommericeel">
             <input
               type="checkbox"
-              name="allowCommercialApproach"
-              checked={values.allowCommercialApproach}
+              id="BenaderingCommericeel"
+              name="BenaderingCommericeel"
+              checked={values.BenaderingCommericeel}
               onChange={handleChange}
               style={styles.checkbox}
             />
+            Sta commerciële benaderingen toe
           </label>
-          <label style={styles.checkboxLabel}>
-            Sta benadering via portal toe:
+          <label style={styles.checkboxLabel} htmlFor="BenaderingViaPortaal">
             <input
               type="checkbox"
-              name="allowPortalApproach"
-              checked={values.allowPortalApproach}
+              id="BenaderingPortal"
+              name="BenaderingPortal"
+              checked={values.BenaderingPortal}
               onChange={handleChange}
               style={styles.checkbox}
             />
+            Sta benadering via portaal toe
           </label>
-          <label style={styles.checkboxLabel}>
-            Sta telefonische benadering toe:
+          <label style={styles.checkboxLabel} htmlFor="BenaderingTelefonisch">
             <input
               type="checkbox"
-              name="allowPhoneApproach"
-              checked={values.allowPhoneApproach}
+              id="BenaderingTelefonisch"
+              name="BenaderingTelefonisch"
+              checked={values.BenaderingTelefonisch}
               onChange={handleChange}
               style={styles.checkbox}
             />
+            Sta telefonische benaderingen toe
           </label>
         </div>
+        {Object.keys(validationErrors).length > 0 && (
+          <div style={styles.errorContainer} role="alert" aria-live="assertive">
+            <p style={styles.errorText}>
+              {Object.values(validationErrors).join(" ")}
+            </p>
+          </div>
+        )}
         <div style={styles.buttonContainer}>
-          <button onClick={onPrev} style={styles.previousButton}>
+          <button
+            onClick={onPrev}
+            style={styles.previousButton}
+            aria-label="Vorige pagina"
+          >
             Vorige pagina
           </button>
-          <button onClick={onSubmit} style={styles.button}>
+          <button
+            onClick={() => handleValidation() && onSubmit()}
+            style={styles.button}
+            aria-label="Afronden Registratie"
+            aria-disabled={Object.keys(validationErrors).length > 0}
+          >
             Afronden Registratie
           </button>
         </div>
@@ -63,7 +94,7 @@ const styles = {
     borderRadius: "8px",
     boxShadow: "0 0 10px rgba(0, 0, 0, 0.1)",
     position: "relative",
-    zIndex: 1 /* Ensure the content is above the triangle background */,
+    zIndex: 1,
   },
   header: {
     color: "#2B50EC",
@@ -71,20 +102,21 @@ const styles = {
     fontWeight: "700",
     marginBottom: "20px",
   },
-  inputContainer: {
-    display: "flex",
-    flexDirection: "column",
-    alignItems: "center",
-    marginBottom: "20px",
-  },
-  label: {
+  checkboxContainer: {
     display: "flex",
     flexDirection: "column",
     alignItems: "flex-start",
     marginBottom: "10px",
   },
+  checkboxLabel: {
+    display: "flex",
+    alignItems: "center",
+    marginBottom: "5px",
+    color: "black",
+    cursor: "pointer",
+  },
   checkbox: {
-    marginTop: "5px", // Adjust the spacing as needed
+    marginLeft: "5px",
   },
   buttonContainer: {
     display: "flex",
@@ -104,30 +136,16 @@ const styles = {
     borderRadius: "5px",
     padding: "5px",
   },
-  checkboxContainer: {
-    display: "flex",
-    flexDirection: "column",
-    alignItems: "center",
+  errorContainer: {
+    backgroundColor: "#FFCCCC",
+    padding: "10px",
+    borderRadius: "4px",
     marginBottom: "10px",
   },
-  
-  checkboxLabel: {
-    display: "flex",
-    alignItems: "center",
-    marginBottom: "5px",
-    color: "black",
-  },
-  
-  checkbox: {
-    marginLeft: "5px",
-  },
-
-  // Media Query for mobile responsiveness
-  "@media only screen and (max-width: 768px)": {
-    container: {
-      width: "80%", // Adjusted width for better responsiveness on smaller screens
-      maxWidth: "none", // Remove the maximum width for smaller screens
-    },
+  errorText: {
+    color: "#2B50EC",
+    fontWeight: 700,
+    fontSize: "1.5rem",
   },
 };
 
--- WPR23-24B/ClientApp/src/components/SignUpComponents/DisabilityType.js
+++ WPR23-24B/ClientApp/src/components/SignUpComponents/DisabilityType.js
@@ -1,55 +1,66 @@
 // DisabilityType.js
 import React from "react";
 import "./SignUpStyle.css";
+import { validateDisabilityType } from "./SignUpValidation";
 
 const DisabilityType = ({ onNext, onPrev, values, handleChange }) => {
+  const [validationErrors, setValidationErrors] = React.useState({});
+
+  const handleValidation = () => {
+    const errors = validateDisabilityType(values);
+    console.log("Values:", values); // Log values to check the state
+
+    if (Object.keys(errors).length > 0) {
+      setValidationErrors(errors);
+      return false;
+    }
+    return true;
+  };
+
   return (
     <div>
-      
-
       {/* Triangle background */}
       <div className="triangle-background"></div>
-
       {/* Main container */}
       <div style={styles.container}>
-        
         {/* Header */}
         <h2 style={styles.header}>Type Beperking</h2>
 
         {/* First input container */}
         <div style={styles.checkboxContainer}>
-          <label style={styles.checkboxLabel}>
-          Fysieke beperking:
+          <label style={styles.checkboxLabel} htmlFor="Fysieke Beperking">
+            Fysieke beperking:
             <input
-              type='checkbox'
-              name="includePhysical"
-              checked={values.includePhysical}
+              type="checkbox"
+              name="FysiekeBeperking"
+              checked={values.VFysiekeBeperking}
               onChange={handleChange}
               style={styles.checkbox}
-              />
-              
+              id="fysiekeBeperkingCheckbox"
+            />
           </label>
-          <label style={styles.checkboxLabel}>
-          Visuele beperking:
+          <label style={styles.checkboxLabel} htmlFor="Visuele Beperking">
+            Visuele beperking:
             <input
-              type='checkbox'
-              name="includeVisual"
-              checked={values.includeVisual}
+              type="checkbox"
+              name="VisueleBeperking"
+              checked={values.VisueleBeperking}
               onChange={handleChange}
               style={styles.checkbox}
-              />
+              id="visueleBeperkingCheckbox"
+            />
           </label>
-          <label style={styles.checkboxLabel}>
-          Auditore beperking:
+          <label style={styles.checkboxLabel} htmlFor="Auditore Beperking">
+            Auditore beperking:
             <input
-              type='checkbox'
-              name="includeAudio"
-              checked={values.includeAudio}
+              type="checkbox"
+              name="AuditieveBeperking"
+              checked={values.AuditieveBeperking}
               onChange={handleChange}
               style={styles.checkbox}
-              />
+              id="auditieveBeperkingCheckbox"
+            />
           </label>
-
         </div>
 
         {/* Second input container for the dropdown */}
@@ -57,33 +68,75 @@ const DisabilityType = ({ onNext, onPrev, values, handleChange }) => {
           <label style={styles.label}>
             Anders, namelijk..
             <select
-              name="beperking"
+              name="OverigeBeperking"
               value={values.beperking}
               onChange={handleChange}
               style={styles.select}
+              aria-invalid={!!validationErrors.OverigeBeperking}
+              aria-describedby="overigeBeperkingError"
             >
-              <option value="">Selecteer</option>
-              <option value="mobility">Beperkte mobiliteit</option>
-              <option value="cognitive">Cognitieve beperking</option>
-              <option value="psychiatric">Psychiatrische beperking</option>
-              <option value="prothese">Prothese van arm of been</option>
+              <option value="" aria-label="Selecteer een beperking">
+                Selecteer
+              </option>
+              <option value="mobility" aria-label="Beperkte mobiliteit">
+                Beperkte mobiliteit
+              </option>
+              <option value="cognitive" aria-label="Cognitieve beperking">
+                Cognitieve beperking
+              </option>
+              <option value="psychiatric" aria-label="Psychiatrische beperking">
+                Psychiatrische beperking
+              </option>
+              <option value="prothese" aria-label="Prothese van arm of been">
+                Prothese van arm of been
+              </option>
               {/* Add more options as needed */}
             </select>
           </label>
+          {validationErrors.OverigeBeperking && (
+            <p
+              style={{
+                color: "#FF0000",
+                marginTop: "5px",
+                fontSize: "1.2rem",
+                width: "400px",
+              }}
+              role="alert"
+              aria-live="assertive"
+              id="overigeBeperkingError"
+            >
+              {validationErrors.OverigeBeperking}
+            </p>
+          )}
         </div>
 
         {/* Button container for navigation */}
         <div style={styles.buttonContainer}>
-          <button onClick={onPrev} style={styles.previousButton}>
+          <button
+            onClick={onPrev}
+            style={styles.previousButton}
+            aria-label="Vorige pagina"
+          >
             Vorige pagina
           </button>
-          <button onClick={onNext} style={styles.button}>
+          <button
+            onClick={() => handleValidation() && onNext()}
+            style={styles.button}
+            aria-label="Volgende pagina"
+          >
             Volgende pagina
           </button>
         </div>
-        
-      </div> {/* End of the main container div */}
-      
+
+        {Object.keys(validationErrors).length > 0 && (
+          <div style={styles.errorContainer}>
+            <p style={styles.errorText}>
+              {Object.values(validationErrors).join(" ")}
+            </p>
+          </div>
+        )}
+      </div>{" "}
+      {/* End of the main container div */}
     </div>
   );
 };
@@ -189,19 +242,31 @@ const styles = {
     alignItems: "center",
     marginBottom: "10px",
   },
-  
+
   checkboxLabel: {
     display: "flex",
     alignItems: "center",
     marginBottom: "5px",
     color: "black",
   },
-  
+
   checkbox: {
     marginLeft: "5px",
   },
-  
-  
+
+  errorContainer: {
+    backgroundColor: "#FFCCCC",
+    padding: "10px",
+    borderRadius: "4px",
+    marginBottom: "10px",
+  },
+
+  errorText: {
+    color: "#2B50EC",
+    fontWeight: 700,
+    fontSize: "1.5rem",
+    justifyContent: "center",
+  },
 };
 
 export default DisabilityType;
