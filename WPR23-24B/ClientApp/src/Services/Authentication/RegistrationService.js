dc6ff123588202693880dfbb47c1cc404c6ae9cf
Lossttt <lfatmi.ayoub@gmail.com>
2024-01-14 21:50:33 +0100

    RegistrationService endpoints aangepast

--- WPR23-24B/ClientApp/src/Services/Authentication/RegistrationService.js
+++ WPR23-24B/ClientApp/src/Services/Authentication/RegistrationService.js
@@ -1,11 +1,20 @@
 import { makeApiRequest } from "../Utils/ApiHelper";
 
-export const handleSignUp = async (userType, formData) => {
+export const handleSignUp = async (userType, formData, isUnder18) => {
   try {
-    const endpoint =
-      userType === "user"
-        ? "Registratie/ervaringsdeskundige"
-        : "Registratie/bedrijf";
+    let endpoint;
+
+    if (userType === "user") {
+      // Determine the appropriate endpoint based on the usertype and user's age
+      endpoint = isUnder18
+        ? "Registratie/ervaringsdeskundige/registerUnder18"
+        : "Registratie/ervaringsdeskundige/registerOver18";
+    } else if (userType === "company") {
+      endpoint = "Registratie/bedrijf/register";
+    } else {
+      throw new Error("Invalid userType");
+    }
+
     const method = "POST";
 
     const response = await makeApiRequest(endpoint, method, formData);
