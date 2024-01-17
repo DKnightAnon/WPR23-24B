d71491b5fee847094853cd1b6a220b48b0c8ed35
Lossttt <lfatmi.ayoub@gmail.com>
2024-01-17 21:03:04 +0100

    UserInfoUpdateModel (nog niet klaar)

---
+++ WPR23-24B/Models/UserInfoUpdateModel.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WPR23_24B.Models
+{
+    public class UserInfoUpdateModel
+    {
+        public string Naam { get; set; }
+        public string Emailadres { get; set; }
+    }
+}
